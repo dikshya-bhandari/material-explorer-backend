aa7ac44073e2a35e4e9c94ece6ef14c6a2674ebc
Dikshya Bhandari <dikshyabhandari05@gmail.com>
2023-11-21 21:31:50 +0545

    api changed to url

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://material-explorer-frontend.vercel.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
