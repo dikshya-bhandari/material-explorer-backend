201e134dde5652f0ed30ac164ec4c24ef17b3e95
Dikshya  Bhandari <dikshyabhandari01@gmail.com>
2024-06-16 22:03:49 +0545

    changed elasticsearch url

--- elasticSetup.js
+++ elasticSetup.js
@@ -1,8 +1,8 @@
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({
-  node: "https://ce8ede5abde54675bb13249907ae9fd1.us-central1.gcp.cloud.es.io:443",
+  node: "https://179b553aee7541cb8a5cb02f44975316.us-central1.gcp.cloud.es.io:443",
   auth: {
-    apiKey: "UkxzTTRZc0JOQVNUSmhaMmhBVUk6ZXRydWNNRjlRYzJ1bF9xdmZ4aUwtdw==",
+    apiKey: "U0N2TElaQUJpYzdwTUpOY3FHbGY6V0lIVElkb1pSZ3VGMFlKbHlmZGRHZw==",
   },
 });
 
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "https://material-explorer-frontend.vercel.app"
+    // "http://localhost:3000"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
